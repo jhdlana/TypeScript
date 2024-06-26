91d226301fe6a2388e68b51f0f232d940c0fede2
jhdlana <ljh06031@gmail.com>
2024-06-26 23:23:58 +0900

    chapter1-type

---
+++ dist/index.js
@@ -0,0 +1,7 @@
+"use strict";
+// console.log("Hellow TypeScript")
+Object.defineProperty(exports, "__esModule", { value: true });
+// const someString = "typeScript!";
+// someString.endsWith("~");
+const util_1 = require("./util");
+(0, util_1.printName)("typeScript");
---
+++ src/index.ts
@@ -0,0 +1,9 @@
+// console.log("Hellow TypeScript")
+
+// const someString = "typeScript!";
+
+// someString.endsWith("~");
+
+import { printName } from "./util";
+
+printName("typeScript")
